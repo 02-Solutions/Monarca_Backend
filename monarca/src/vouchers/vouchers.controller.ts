8a8b08c92697ca753d5cd1c2b4970c040d7c0ee0
AlarconNicolas <alarcon.panopoulou@gmail.com>
2025-04-29 18:52:50 -0600

    Se añadio dos CRUD a request

--- monarca/src/vouchers/vouchers.controller.ts
+++ monarca/src/vouchers/vouchers.controller.ts
@@ -9,8 +9,8 @@ import {
   Patch,
 } from '@nestjs/common';
 import { VouchersService } from './vouchers.service';
-import { CreateVoucherDto } from './dto/Create-Voucher-dto';
-import { UpdateVoucherDto } from './dto/Update-Voucher-dto';
+import { CreateVoucherDto } from './dto/create-voucher-dto';
+import { UpdateVoucherDto } from './dto/update-voucher-dto';
 import { Voucher } from './entities/vouchers.entity';
 import { ApiTags } from '@nestjs/swagger';
 
