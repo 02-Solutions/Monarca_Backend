fbd5db56b282a55b83803a2a172a922bbaf84de7
Imanol Santisteban <a01783637@tec.mx>
2025-05-13 18:32:42 -0600

    Line change

--- monarca/src/vouchers/dto/update-voucher-dto.ts
+++ monarca/src/vouchers/dto/update-voucher-dto.ts
@@ -1,4 +1,4 @@
-import { CreateVoucherDto } from './Create-Voucher-dto';
+import { CreateVoucherDto } from './create-voucher-dto';
 import { PartialType } from '@nestjs/swagger';
 
 export class UpdateVoucherDto extends PartialType(CreateVoucherDto) {}
