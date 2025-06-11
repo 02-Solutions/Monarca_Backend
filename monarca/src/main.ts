8158d8d270ed3dcaa1a500a97e5fb6019ba4effa
Remy Patgher <a01784177@tec.mx>
2025-06-11 12:37:13 -0600

    fix(origin): cors origin set with an env

--- monarca/src/main.ts
+++ monarca/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   // Habilitar CORS para permitir peticiones desde el origen del frontend
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
